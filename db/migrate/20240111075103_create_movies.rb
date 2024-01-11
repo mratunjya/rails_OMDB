b19fd5336a7cf710125dcb84cb520af0107f0b28
Mratunjya Shankhdhar <mratunjya13@gmail.com>
2024-01-11 22:56:54 +0530

    Movie Model Created

---
+++ db/migrate/20240111075103_create_movies.rb
@@ -0,0 +1,22 @@
+class CreateMovies < ActiveRecord::Migration[7.1]
+  def change
+    create_table :movies, id: false do |t|
+      t.string :imdb_id, null: false, primary_key: true
+      t.string :awards
+      t.string :imdb_rating
+      t.string :imdb_votes
+      t.string :media_type
+      t.string :metascore
+      t.text :plot
+      t.text :poster
+      t.string :rated
+      t.string :released
+      t.string :runtime
+      t.string :title
+      t.string :total_seasons
+      t.integer :year
+
+      t.timestamps
+    end
+  end
+end
