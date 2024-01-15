2794daa70b28b6666dac56cbd8c6aa2e2f61bee5
Mratunjya Shankhdhar <mratunjya13@gmail.com>
2024-01-15 16:52:18 +0530

    Movies Controller Added

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 
+  root "movies#index"
+
   namespace :api do
     resources :movies,  only: [:index]
   end
