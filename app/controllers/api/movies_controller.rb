1a11c25ab7aa31da5a4420e3a32a4a3a73313be4
Mratunjya Shankhdhar <mratunjya13@gmail.com>
2024-01-15 11:55:00 +0530

    Optimize Get Movies API for SQL queries

--- app/controllers/api/movies_controller.rb
+++ app/controllers/api/movies_controller.rb
@@ -8,29 +8,29 @@ class Api::MoviesController < ApplicationController
     page = params[:page].present? ? params[:page].to_i : 1
     per_page = 8
 
-    movies = Movie
-              .left_joins(:movie_actor, :movie_genre)
-              .where('movies.title LIKE ? AND movies.released LIKE ? AND movie_actors.name LIKE ? AND movie_genres.genre LIKE ?', title, released, actor, genre)
-              .distinct
+    movies_query = Movie
+                    .left_joins(:movie_actor, :movie_genre)
+                    .where('movies.title LIKE ? AND movies.released LIKE ? AND movie_actors.name LIKE ? AND movie_genres.genre LIKE ?', "%#{params[:title]}%", "%#{params[:released]}%", "%#{params[:actor]}%", "%#{params[:genre]}%")
+                    .distinct
 
-    total_count = movies.count
+    total_count = movies_query.count
 
-    if page * per_page - total_count >= 8
-      render json: {error: "No movies present on page: #{page}", total_count: total_count}
+    if page * per_page - total_count >= per_page
+      render json: { error: "No movies present on page: #{page}", total_count: total_count }
       return
     end
 
-    movies = movies.offset((page - 1) * per_page)
-                    .limit(per_page)
+    movies = movies_query
+              .offset((page - 1) * per_page)
+              .limit(per_page)
+              .includes(:movie_actor, :movie_genre)
+    return
 
     output = movies.map do |movie|
-      actors = MovieActor.where(movie_id: movie.id).pluck(:name)
-      genres = MovieGenre.where(movie_id: movie.id).pluck(:genre)
-
       {
-        actors: actors,
+        actors: movie.movie_actor.pluck(:name),
         awards: movie.awards,
-        genre: genres,
+        genre: movie.movie_genre.pluck(:genre),
         imdb_rating: movie.imdb_rating,
         imdb_votes: movie.imdb_votes,
         media_type: movie.media_type,
