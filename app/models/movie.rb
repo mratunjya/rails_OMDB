c9d8a59fdffcbe2f902abf2a76fbc3435f5217d4
Mratunjya Shankhdhar <mratunjya13@gmail.com>
2024-01-18 13:49:55 +0530

    Added Inverse of Model Association, accepts_nested_attributes_for added in movie model
    When we declare inverse_of on an association, it tells Rails that the association on the other side also knows about this association, and it helps to avoid unnecessary database queries.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,9 +1,11 @@
 class Movie < ApplicationRecord
-    has_many :movie_actors, foreign_key: "movie_id"
-    has_many :movie_countries, foreign_key: "movie_id"
-    has_many :movie_directors, foreign_key: "movie_id"
-    has_many :movie_genres, foreign_key: "movie_id"
-    has_many :movie_languages, foreign_key: "movie_id"
-    has_many :movie_ratings, foreign_key: "movie_id"
-    has_many :movie_writers, foreign_key: "movie_id"
+    has_many :movie_actors, foreign_key: "movie_id", inverse_of: :movie
+    has_many :movie_countries, foreign_key: "movie_id", inverse_of: :movie
+    has_many :movie_directors, foreign_key: "movie_id", inverse_of: :movie
+    has_many :movie_genres, foreign_key: "movie_id", inverse_of: :movie
+    has_many :movie_languages, foreign_key: "movie_id", inverse_of: :movie
+    has_many :movie_ratings, foreign_key: "movie_id", inverse_of: :movie
+    has_many :movie_writers, foreign_key: "movie_id", inverse_of: :movie
+
+    accepts_nested_attributes_for :movie_actors, :movie_countries, :movie_directors, :movie_genres, :movie_languages, :movie_ratings, :movie_writers, :movie_writers
 end
--- app/models/movie_actor.rb
+++ app/models/movie_actor.rb
@@ -1,2 +1,3 @@
 class MovieActor < ApplicationRecord
+    belongs_to :movie, inverse_of: :movie_actors
 end
